1e2976794bbb04a8392484d0a36d045db023f305
T0ast <minhthinle69@gmail.com>
2026-04-04 13:35:48 +0700

    Implemented Road class

---
+++ app/src/main/java/org/Road.java
@@ -0,0 +1,52 @@
+package org;
+
+import java.util.PriorityQueue;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+import org.vehicles.*;
+
+public class Road {
+    private static final int MAX_VEHICLES = 2;
+    private static final int RADIUS = 20;
+    private PriorityQueue<Vehicles> priorityQueue;
+    private Vehicles[] vehicles;
+    private Vector2 position;
+    
+    public Road(Vector2 position) {
+        this.priorityQueue = new PriorityQueue<Vehicles>();
+        this.vehicles = new Vehicles[MAX_VEHICLES];
+        this.position = position;
+    }
+
+    public void draw(DrawMode drawMode, ShapeRenderer shapeRenderer) {
+        switch (drawMode) {
+            case DrawMode.GRAPHICAL:
+                graphicalDraw();
+                break;
+
+            case DrawMode.PRIMITIVE:
+                primitiveDraw(shapeRenderer);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void graphicalDraw() {
+        // TODO: Implement graphical draw
+    }
+
+    public void primitiveDraw(ShapeRenderer shapeRenderer) {
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.circle(position.x, position.y, RADIUS);
+    }
+
+    public Vector2 getPosition() {
+        return new Vector2(position);
+    }
+}
+
