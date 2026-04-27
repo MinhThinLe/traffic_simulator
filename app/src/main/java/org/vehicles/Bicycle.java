bc44cd22b6a06642784ce00a66a663d76cbe7c5b
T0ast <minhthinle69@gmail.com>
2026-04-27 10:58:53 +0700

    bicycles now render text along with their shape

--- app/src/main/java/org/vehicles/Bicycle.java
+++ app/src/main/java/org/vehicles/Bicycle.java
@@ -9,11 +9,14 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 
 public class Bicycle extends Vehicle {
     private static final float DEFAULT_BICYCLE_SPEED = 50;
-    private static final float WIDTH = 10;
-    private static final float HEIGHT = 20;
+    private static final float WIDTH = 20;
+    private static final float HEIGHT = 10;
     private static final float[] polygonMesh = new float[] {
         -WIDTH / 2, -HEIGHT / 2,
          WIDTH / 2, -HEIGHT / 2,
@@ -38,17 +41,49 @@ public class Bicycle extends Vehicle {
 
     @Override
     protected void primitiveDraw() {
-        Vector2 destination = nextDestination().getPosition();
-        Vector2 direction = destination.sub(position);
-
-        float angle = direction.angleDeg();
+        float angle = getDirection();
         Polygon polygon = new Polygon(polygonMesh);
-        polygon.rotate(angle - 90);
+
+        polygon.rotate(angle);
         polygon.translate(position.x, position.y);
 
         ShapeRenderer shapeRenderer = Renderer.primitiveRenderer;
 
         shapeRenderer.setColor(Color.BLACK);
         shapeRenderer.polygon(polygon.getTransformedVertices());
+
+        drawText();
+    }
+
+    private float getDirection() {
+        Vector2 destination = nextDestination().getPosition();
+        Vector2 direction = destination.sub(position);
+
+        return direction.angleDeg();
+    }
+
+    private void drawText() {
+        LabelStyle style = new LabelStyle(Renderer.textRenderer, Color.RED);
+        Label text = new Label("Bicycle", style);
+
+        Container<Label> container = new Container<>(text);
+
+        container.setTransform(true);
+        container.setX(position.x);
+        container.setY(position.y);
+
+        float scaleX = WIDTH / text.getWidth();
+        float scaleY = HEIGHT / text.getHeight(); 
+
+        container.setScaleX(scaleX);
+        container.setScaleY(scaleY);
+
+        float angle = getDirection();
+        if (angle > 90 && angle < 270) {
+            angle -= 180;
+        }
+        container.setRotation(angle);
+
+        container.draw(Renderer.graphicalRenderer, 1);
     }
 }
