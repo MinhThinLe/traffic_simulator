e1f7c3c9bfe82e4ff8b7b271e782b6df67615d8a
T0ast <minhthinle69@gmail.com>
2026-04-29 13:59:11 +0700

    Removed unnecessary codes

--- app/src/main/java/org/render/ui/RenderModeButton.java
+++ app/src/main/java/org/render/ui/RenderModeButton.java
@@ -1,37 +1,14 @@
 package org.render.ui;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 import org.render.DrawMode;
 
 public class RenderModeButton extends TextButton {
-    private boolean clicked;
-
     public RenderModeButton(BitmapFont font, Skin skin) {
         super(DrawMode.PRIMITIVE.toString(), makeStyle(skin, font));
-
-        addListener(
-                new InputListener() {
-                    @Override
-                    public boolean touchDown(
-                            InputEvent event, float x, float y, int pointer, int button) {
-                        clicked = !clicked;
-                        return true;
-                    }
-                });
-    }
-
-    public boolean shouldFlip() {
-        if (clicked) {
-            clicked = false;
-            return true;
-        }
-
-        return false;
     }
 
     private static TextButtonStyle makeStyle(Skin skin, BitmapFont font) {
