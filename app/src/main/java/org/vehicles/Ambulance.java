cea81c5029140ae06c1e50bc3006201e64939992
T0ast <minhthinle69@gmail.com>
2026-05-10 15:48:09 +0700

    Implemented ambulance

---
+++ app/src/main/java/org/vehicles/Ambulance.java
@@ -0,0 +1,42 @@
+package org.vehicles;
+
+import com.badlogic.gdx.math.Vector2;
+import org.road.Road;
+
+import java.util.List;
+
+public class Ambulance extends Vehicle {
+    private static final float DEFAULT_AMBULANCE_SPEED = 100;
+    private static final float WIDTH = 30;
+    private static final float HEIGHT = 15;
+
+    public Ambulance(List<Road> path, Vector2 position) {
+        super(path, position, DrivingMode.AGGRESSIVE, DEFAULT_AMBULANCE_SPEED);
+    }
+
+    @Override
+    public int getVehiclePriority() {
+        return 10;
+    }
+
+    @Override
+    public float getWidth() {
+        return WIDTH;
+    }
+
+    @Override
+    public float getHeight() {
+        return HEIGHT;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return "Ambulance";
+    }
+
+    @Override
+    protected void graphicalDraw() {
+        // TODO Auto-generated method stub
+        
+    }
+}
