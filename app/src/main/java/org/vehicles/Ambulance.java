3c73fd387cbb3139a9dc2d4f857961f917b4cec1
T0ast <minhthinle69@gmail.com>
2026-05-15 22:05:53 +0700

    Game development is my passion

--- app/src/main/java/org/vehicles/Ambulance.java
+++ app/src/main/java/org/vehicles/Ambulance.java
@@ -3,7 +3,6 @@ package org.vehicles;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 
 import org.render.drawcalls.TextureDrawCall;
 import org.road.Road;
@@ -21,13 +20,15 @@ public class Ambulance extends Vehicle {
     static final int RENDERED_SIZE = 50;
 
     private Sprite sprite;
-    private AmbulanceLight light;
+    private Timer timer;
+    private boolean side;
+
 
     public Ambulance(List<Road> path) {
         super(path, DrivingMode.AGGRESSIVE, DEFAULT_AMBULANCE_SPEED, 1f, 1f);
         Texture ambulanceTexture = new Texture(Gdx.files.internal(TEXTURE_PATH));
         sprite = new Sprite(ambulanceTexture);
-        light = new AmbulanceLight();
+        timer = new Timer(0.2f);
     }
 
     @Override
@@ -51,16 +52,24 @@ public class Ambulance extends Vehicle {
     }
 
     private static final int SPRITE_COUNT = 48;
-    private static final int GRID_WIDTH = 7;
+    private static final int COLUMN_COUNT = 7;
 
     @Override
     protected void graphicalDraw() {
+        timer.tick();
+        if (timer.hasFinished()) {
+            side = !side;
+        }
         float direction = getDirection().angleDeg();
         float spriteStep = 360f / (SPRITE_COUNT - 1);
         int spriteNumber = SPRITE_COUNT - (int) (direction / spriteStep) - 1;
 
-        int regionX = (spriteNumber % GRID_WIDTH) * SPRITE_SIZE;
-        int regionY = (spriteNumber / GRID_WIDTH) * SPRITE_SIZE;
+        int regionX = (spriteNumber % COLUMN_COUNT) * SPRITE_SIZE;
+        int regionY = (spriteNumber / COLUMN_COUNT) * SPRITE_SIZE;
+
+        if (side) {
+            regionX += COLUMN_COUNT * SPRITE_SIZE;
+        }
 
         sprite.setRegion(regionX, regionY, SPRITE_SIZE, SPRITE_SIZE);
 
@@ -72,50 +81,5 @@ public class Ambulance extends Vehicle {
                         RENDERED_SIZE,
                         RENDERED_SIZE)
                 .submit();
-
-        this.light.draw(this.position, this.direction.angleDeg());
-    }
-}
-
-class AmbulanceLight {
-    private static final String TEXTURE_PATH = "org/vehicles/textures/ambulance/light.png";
-
-    private static final int RENDERED_SIZE = 16;
-
-    private static final Vector2 LEFT_LIGHT_OFFSET = new Vector2(11, -16).scl(Ambulance.RENDERED_SIZE / Ambulance.SPRITE_SIZE);
-    private static final Vector2 RIGHT_LIGHT_OFFSET = new Vector2(11, 6).scl(Ambulance.RENDERED_SIZE / Ambulance.SPRITE_SIZE);
-    
-    private Sprite sprite;
-    private Timer timer;
-    private int renderState;
-    AmbulanceLight() {
-        Texture texture = new Texture(Gdx.files.internal(TEXTURE_PATH));
-        this.sprite = new Sprite(texture);
-        this.timer = new Timer(0.2f);
-    }
-
-    public void draw(Vector2 parentPosition, float rotation) {
-        timer.tick();
-        if (timer.hasFinished()) {
-            renderState = (renderState + 1) % 4;
-        }
-        if (renderState % 2 == 0) {
-            return;
-        }
-
-        Vector2 offset = switch (renderState) {
-            // case 1 -> LEFT_LIGHT_OFFSET;
-            // case 3 -> RIGHT_LIGHT_OFFSET;
-            default -> Vector2.Zero;
-        };
-
-        offset.rotateDeg(rotation);
-
-        new TextureDrawCall(sprite, 
-                parentPosition.x - RENDERED_SIZE / 2 + offset.x, 
-                parentPosition.y - RENDERED_SIZE / 2 + offset.y, 
-                -parentPosition.y, 
-                RENDERED_SIZE, 
-                RENDERED_SIZE).submit();;
     }
 }
