7d6c01c324433920015069972c730b1e4a15bada
T0ast <minhthinle69@gmail.com>
2026-05-18 16:45:07 +0700

    Added getThumbnail method to VehicleFactory

--- app/src/main/java/org/vehicles/VehicleFactory.java
+++ app/src/main/java/org/vehicles/VehicleFactory.java
@@ -2,8 +2,11 @@ package org.vehicles;
 
 import org.road.Road;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 import java.util.List;
 
 public interface VehicleFactory {
     public Vehicle createVehicle(List<Road> path);
+    public Sprite getThumbnail();
 }
