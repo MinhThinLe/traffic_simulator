3c9ba8dc4ad18c2bc8cef4761d060178059ecd1e
T0ast <minhthinle69@gmail.com>
2026-05-21 14:41:31 +0700

    Added a button that loads a custom map

--- app/src/main/java/org/render/ui/MapSelection.java
+++ app/src/main/java/org/render/ui/MapSelection.java
@@ -1,11 +1,15 @@
 package org.render.ui;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFileChooser;
 
 public class MapSelection implements Gui {
     private static final String[] MAPS = {
@@ -39,6 +43,19 @@ public class MapSelection implements Gui {
         for (int i = 0; i < MAPS.length; i++) {
             table.add(buttons.get(i)).size(300, 50).pad(10).row();
         }
+
+        TextButton customMapButton = new TextButton("Nạp từ file", Styles.getButtonStyle());
+        customMapButton.setName("level selection button");
+        table.add(customMapButton).size(300, 50).pad(10).row();
+        customMapButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.showOpenDialog(null);
+                System.out.println(chooser.getSelectedFile().toString());
+                customMapButton.setUserObject(chooser.getSelectedFile().toString());
+            }
+        });
 
         return table;
     }
