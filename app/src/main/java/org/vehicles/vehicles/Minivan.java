f8b956643b05212644dbd71a53b027998e4b60b8
T0ast <minhthinle69@gmail.com>
2026-05-21 18:11:45 +0700

    Made minivans a tiny bit larger

--- app/src/main/java/org/vehicles/vehicles/Minivan.java
+++ app/src/main/java/org/vehicles/vehicles/Minivan.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Minivan extends Vehicle {
     private static final String TEXTURE_PATH = "org/vehicles/textures/minivans/";
     private static final float DEFAULT_SPEED = 45;
-    private static final float WIDTH = 30;
+    private static final float WIDTH = 35;
     private static final float HEIGHT = 15;
 
     private static final int SPRITE_SIZE = 100;
